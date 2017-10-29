ceb41781b49d98e5e4c4f2fcd4c7a0e8e9d1a065
Zumium <martin007323@gmail.com>
2017-10-29 19:20:48 +0800

    rearrange source file directory structure

--- brovicam/brovi_cam.go
+++ brovicam/brovi_cam.go
@@ -4,21 +4,51 @@ package main
 #include "brovi_cam.h"
 */
 import "C"
+import "errors"
+
+var (
+	//ErrOpenCamFail 打开摄像头失败
+	ErrOpenCamFail = errors.New("failed to open camera")
+	//ErrStartFail 启动视频失败
+	ErrStartFail = errors.New("failed to start")
+	//ErrStopFail 停止视频失败
+	ErrStopFail = errors.New("failed to stop")
+	//ErrGetNextBufferFail 获取下一帧失败
+	ErrGetNextBufferFail = errors.New("failed to get next buffer")
+)
 
 //BroviCam handles the video capturing process
 type BroviCam struct {
 	broviCam *C.BroviCam
 }
 
-func newBroviCam(config *C.BroviCamConfig) *BroviCam {
+func newBroviCam(config *C.BroviCamConfig) (*BroviCam, error) {
 	bc := new(BroviCam)
-	bc.broviCam = C.BroviCam_Open(config)
-	return bc
+	if bc.broviCam = C.BroviCam_Open(config); bc.broviCam == nil {
+		return nil, ErrOpenCamFail
+	}
+	return bc, nil
 }
 
 //Close closes the camera file and destroys underlying dependency
 func (bc *BroviCam) Close() {
 	C.BroviCam_Close(bc.broviCam)
+}
+
+//Start starts the video stream
+func (bc *BroviCam) Start() error {
+	if int(C.BroviCam_Start(bc.broviCam)) < 0 {
+		return ErrStartCamFail
+	}
+	return nil
+}
+
+//Stop stops the video stream
+func (bc *BroviCam) Stop() error {
+	if int(C.BroviCam_Stop(bc.broviCam) < 0) {
+		return ErrStopFail
+	}
+	return nil
 }
 
 //--------------------------------------------------
@@ -40,7 +70,7 @@ func NewBroviCam(devfile string) *BroviCamBuilder {
 }
 
 //Open builds the actual BroviCam object and start intializing process
-func (builder *BroviCamBuilder) Open() *BroviCam {
+func (builder *BroviCamBuilder) Open() (*BroviCam, error) {
 	return newBroviCam(builder.config)
 }
 
